4128343e4101526333bd93a4bee03898b1d14947
Леонид <leonid.chugay@yandex.ru>
2020-09-24 21:55:17 +0300

    + Line

---
+++ src/com/metanit/Main.java
@@ -0,0 +1,9 @@
+package com.metanit;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World!!!");
+	System.out.println("Line");// write your code here
+    }
+}
